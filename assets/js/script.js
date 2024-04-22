464c8e7c2a555d194da6699bae711ebb2c3440fb
stef-von-d <stefania.pastramagiu@yahoo.com>
2024-04-22 11:32:58 +0000

    questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("Connected!")
+const quizData = [
+    { question: "Which language has the more native speakers?" ,
+    a: "English",
+    b: "Spanish",
+    c: "Chinese" ,
+    d: "French" ,
+    correct: "b"
+    }
+    {
+        question: "What is the capital of Romania?"
+        a:"London",
+        b:"Paris", 
+        c:"Barcelona", 
+        d:"Bucharest",
+        correct: "d"
+    }
+    {
+        question:"How many planets are in the solar system?"
+        a:  
+    }
+]
